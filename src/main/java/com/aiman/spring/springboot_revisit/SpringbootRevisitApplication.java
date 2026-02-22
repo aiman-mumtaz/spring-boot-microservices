79e6dab5e09493ba44a65294122771e21e3e8597
Aiman Mumtaz <aimanmumtaz786@gmail.com>
2026-02-22 21:19:29 +0530

    Initialise Spring project

---
+++ src/main/java/com/aiman/spring/springboot_revisit/SpringbootRevisitApplication.java
@@ -0,0 +1,13 @@
+package com.aiman.spring.springboot_revisit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRevisitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRevisitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiman/spring/springboot_revisit/SpringbootRevisitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiman.spring.springboot_revisit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRevisitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
