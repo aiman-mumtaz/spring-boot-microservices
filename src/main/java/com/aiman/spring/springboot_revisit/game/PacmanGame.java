d52bd078ac7d58ba1a119286fdc4a4c553f44f5e
Aiman Mumtaz <aimanmumtaz786@gmail.com>
2026-02-26 13:52:56 +0530

    Understand @Primary annotation

--- src/main/java/com/aiman/spring/springboot_revisit/game/PacmanGame.java
+++ src/main/java/com/aiman/spring/springboot_revisit/game/PacmanGame.java
@@ -1,5 +1,8 @@
 package com.aiman.spring.springboot_revisit.game;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PacmanGame implements GamingConsole{
     public void up() {
         System.out.println("PacmanGame Jump");
